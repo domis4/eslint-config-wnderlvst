dce89e4f68486884f30eec42a9cbbedd35810afe
Dominik Stötter <dominik.stoetter@yahoo.com>
2021-11-03 13:01:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# eslint-config-wnderlvst
+
+## Installation
+
+```
+# Install `eslint-config-wnderlvst`:
+$ yarn add -D eslint-config-wnderlvst
+```
+
+## Usage
+
+Once you installed `eslint-config-wnderlvst`, you can use it by specifying `wnderlvst` in the [`extends`](http://eslint.org/docs/user-guide/configuring#extending-configuration-files) section of your [ESLint configuration](http://eslint.org/docs/user-guide/configuring).
+
+```js
+{
+  "extends": "wnderlvst",
+  "rules": {
+    // Additional, per-project rules...
+  }
+}
+```
+
---
+++ eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['eslint:recommended', 'standard', 'prettier'],
+  plugins: [ 'jest', 'prettier', 'require-path-exists'],
+  rules: {
+    'arrow-parens': ['error', 'as-needed'],
+    curly: ['error', 'all'],
+    'max-len': [
+      'error',
+      120,
+      2,
+      {
+        ignoreRegExpLiterals: true,
+        ignoreUrls: true,
+      },
+    ],
+
+    'array-bracket-spacing': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+
+    'require-path-exists/notEmpty': 'error',
+    'require-path-exists/tooManyArguments': 'error',
+    'require-path-exists/exists': 'error',
+
+    'prettier/prettier': ['error', require('./prettier.config')],
+
+    'node/no-unpublished-require': 'off',
+    'node/no-unsupported-features': 'off',
+  },
+}
