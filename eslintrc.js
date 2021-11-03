4eb07fecbdca15c70619e04bad2773c5c0e5f040
Dominik Stötter <dominik.stoetter@yahoo.com>
2021-11-03 14:01:00 +0100

    prettier fixes

--- eslintrc.js
+++ eslintrc.js
@@ -25,8 +25,6 @@ module.exports = {
     'require-path-exists/tooManyArguments': 'error',
     'require-path-exists/exists': 'error',
 
-    'prettier/prettier': ['error', require('./prettier.config')],
-
     'node/no-unpublished-require': 'off',
     'node/no-unsupported-features': 'off',
   },
